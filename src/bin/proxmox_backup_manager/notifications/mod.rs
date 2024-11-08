aacbda25fe03848a76eb442aca2e7cee1a812574
Lukas Wagner <l.wagner@proxmox.com>
2024-11-08 15:41:22 +0100

    management cli: add CLI for webhook targets
    The code was copied and adapted from the gotify target CLI.
    
    Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

--- src/bin/proxmox_backup_manager/notifications/mod.rs
+++ src/bin/proxmox_backup_manager/notifications/mod.rs
@@ -5,12 +5,14 @@ mod matchers;
 mod sendmail;
 mod smtp;
 mod targets;
+mod webhook;
 
 pub fn notification_commands() -> CommandLineInterface {
     let endpoint_def = CliCommandMap::new()
         .insert("gotify", gotify::commands())
         .insert("sendmail", sendmail::commands())
-        .insert("smtp", smtp::commands());
+        .insert("smtp", smtp::commands())
+        .insert("webhook", webhook::commands());
 
     let cmd_def = CliCommandMap::new()
         .insert("endpoint", endpoint_def)
