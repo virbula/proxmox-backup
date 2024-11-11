7e983053f3f36bdcfca96cbe70f4271d124545e4
Christian Ebner <c.ebner@proxmox.com>
2024-11-11 16:43:36 +0100

    api/api-types: refactor api endpoint version, add api types
    Add a dedicated api type for the `version` api endpoint and helper
    methods for supported feature comparison.
    This will be used to detect api incompatibility of older hosts, not
    supporting some features.
    
    Use the new api type to refactor the version endpoint and set it as
    return type.
    
    Signed-off-by: Christian Ebner <c.ebner@proxmox.com>

--- src/api2/version.rs
+++ src/api2/version.rs
@@ -1,27 +1,35 @@
 //! Version information
 
 use anyhow::Error;
-use serde_json::{json, Value};
+use serde_json::Value;
 
-use proxmox_router::{ApiHandler, ApiMethod, Permission, Router, RpcEnvironment};
-use proxmox_schema::ObjectSchema;
+use proxmox_router::{ApiMethod, Permission, Router, RpcEnvironment};
+use proxmox_schema::api;
 
-fn get_version(
+use pbs_api_types::ApiVersionInfo;
+
+const FEATURES: &'static [&'static str] = &[];
+
+#[api(
+    returns: {
+        type: ApiVersionInfo,
+    },
+    access: {
+        permission: &Permission::Anybody,
+    }
+)]
+///Proxmox Backup Server API version.
+fn version(
     _param: Value,
     _info: &ApiMethod,
     _rpcenv: &mut dyn RpcEnvironment,
-) -> Result<Value, Error> {
-    Ok(json!({
-        "version": pbs_buildcfg::PROXMOX_PKG_VERSION,
-        "release": pbs_buildcfg::PROXMOX_PKG_RELEASE,
-        "repoid": pbs_buildcfg::PROXMOX_PKG_REPOID
-    }))
+) -> Result<ApiVersionInfo, Error> {
+    Ok(ApiVersionInfo {
+        version: pbs_buildcfg::PROXMOX_PKG_VERSION.to_string(),
+        release: pbs_buildcfg::PROXMOX_PKG_RELEASE.to_string(),
+        repoid: pbs_buildcfg::PROXMOX_PKG_REPOID.to_string(),
+        features: FEATURES.iter().map(|feature| feature.to_string()).collect(),
+    })
 }
 
-pub const ROUTER: Router = Router::new().get(
-    &ApiMethod::new(
-        &ApiHandler::Sync(&get_version),
-        &ObjectSchema::new("Proxmox Backup Server API version.", &[]),
-    )
-    .access(None, &Permission::Anybody),
-);
+pub const ROUTER: Router = Router::new().get(&API_METHOD_VERSION);
