virbula/proxmox-backup

commit b46281e9516a537d6625e94b5775c3cebcd3ae58

ui: only add delete parameter on token edit, not when creating tokens
otherwise tokens without comments can no longer be created as the api
will reject the additional `delete` parameter. this bug was introduced
by commit:

6a745f4: api: token: make comment deletable
Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>

www/window/TokenEdit.js
@@ -68,8 +68,10 @@ Ext.define('PBS.window.TokenEdit', {
 	    {
 		xtype: 'proxmoxtextfield',
 		name: 'comment',
-		deleteEmpty: true,
 		fieldLabel: gettext('Comment'),
+		cbind: {
+		    deleteEmpty: "{!isCreate}"
+		},
 	    },
 	],
     },

commit efc721e79fb9a79f40b919115ed277ed0efb1dc4

ui: token edit: fix missing trailing-comma
Fixes: b46281e9 ("ui: only add delete parameter on token edit, not when creating tokens")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/window/TokenEdit.js
@@ -70,7 +70,7 @@ Ext.define('PBS.window.TokenEdit', {
 		name: 'comment',
 		fieldLabel: gettext('Comment'),
 		cbind: {
-		    deleteEmpty: "{!isCreate}"
+		    deleteEmpty: "{!isCreate}",
 		},
 	    },
 	],