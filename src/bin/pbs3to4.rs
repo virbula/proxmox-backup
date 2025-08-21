67b501aaddc5401487ef4f7ff0481d02b4ebab66
Stoiko Ivanov <s.ivanov@proxmox.com>
2025-08-21 16:17:18 +0200

    pbs3to4: use boolean variable for systemd-boot installation state
    while it's not saving too much run-time wise (each branch stats the
    file only once), it's a bit easier to read.
    
    Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
    Reviewed-by: Shannon Sterz <s.sterz@proxmox.com>
    Tested-by: Shannon Sterz <s.sterz@proxmox.com>
    Link: https://lore.proxmox.com/20250821141719.4130062-3-s.ivanov@proxmox.com

--- src/bin/pbs3to4.rs
+++ src/bin/pbs3to4.rs
@@ -206,8 +206,11 @@ impl Checker {
         self.output
             .log_info("Checking bootloader configuration...")?;
 
+        let sd_boot_installed =
+            Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file();
+
         if !Path::new("/sys/firmware/efi").is_dir() {
-            if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+            if sd_boot_installed {
                 self.output.log_info(
                     "systemd-boot package installed on legacy-boot system is not \
                     necessary, consider removing it",
@@ -227,7 +230,7 @@ impl Checker {
                     .log_skip("not yet upgraded, systemd-boot still needed for bootctl")?;
                 return Ok(());
             }
-            if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+            if sd_boot_installed {
                 self.output.log_fail(
                     "systemd-boot meta-package installed. This will cause issues on upgrades of \
                     boot-related packages.\n\
@@ -237,7 +240,7 @@ impl Checker {
                 return Ok(());
             }
         } else {
-            if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+            if sd_boot_installed {
                 self.output.log_fail(
                     "systemd-boot meta-package installed. This will cause problems on upgrades of other \
                     boot-related packages.\n\
