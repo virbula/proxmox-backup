virbula/proxmox-backup

commit 3ae23e7137af12658312554c6b943037c4ce981d

bin: pbs3to4: adapt boot-loader checks to trixie
basically carry-over the checks from pve-manger for pve8to9 [0]:
* 65ffcdd0 ("cli: pve8to9: rework boot-loader suggestions for trixie")
* 7cc36772 ("8 to 9 checks: do not ask bootctl if systemd-boot is used.")
* 2d79b567 ("8 to 9 checks: check for removable grub-install")

Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/all/20250811091135.127299-1-s.ivanov@proxmox.com

src/bin/pbs3to4.rs
@@ -207,39 +207,71 @@ impl Checker {
             .log_info("Checking bootloader configuration...")?;
 
         if !Path::new("/sys/firmware/efi").is_dir() {
+            if !Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+                self.output.log_info(
+                    "systemd-boot package installed on legacy-boot system is not \
+                        necessary, consider removing it",
+                )?;
+                return Ok(());
+            }
             self.output
-                .log_skip("System booted in legacy-mode - no need for systemd-boot")?;
+                .log_skip("System booted in legacy-mode - no need for additional pacckages.")?;
             return Ok(());
         }
 
+        let mut boot_ok = true;
         if Path::new("/etc/kernel/proxmox-boot-uuids").is_file() {
             // PBS packages version check needs to be run before
             if !self.upgraded {
                 self.output
-                    .log_skip("not yet upgraded, no need to check the presence of systemd-boot")?;
+                    .log_skip("not yet upgraded, systemd-boot still needed for bootctl")?;
                 return Ok(());
             }
             if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
-                self.output
-                    .log_pass("bootloader packages installed correctly")?;
+                self.output.log_fail( "systemd-boot meta-package installed. This will cause issues on upgrades of \
+                    boot-related packages. Install 'systemd-boot-efi' and 'systemd-boot-tools' explicitly \
+                and remove 'systemd-boot'")?;
                 return Ok(());
             }
-            self.output.log_warn(
-                "proxmox-boot-tool is used for bootloader configuration in uefi mode \
-                 but the separate systemd-boot package, is not installed.\n\
-                 initializing new ESPs will not work until the package is installed.",
-            )?;
-            return Ok(());
-        } else if !Path::new("/usr/share/doc/grub-efi-amd64/changelog.Debian.gz").is_file() {
-            self.output.log_warn(
-                "System booted in uefi mode but grub-efi-amd64 meta-package not installed, \
-             new grub versions will not be installed to /boot/efi!
-             Install grub-efi-amd64.",
-            )?;
-            return Ok(());
         } else {
-            self.output
-                .log_pass("bootloader packages installed correctly")?;
+            if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+                self.output.log_fail( "systemd-boot meta-package installed. This will cause problems on upgrades of other \
+                        boot-related packages. Remove 'systemd-boot' See \
+                        https://pbs.proxmox.com/wiki/Upgrade_from_3_to_4#sd-boot-warning for more information."
+                )?;
+                boot_ok = false;
+            }
+            if !Path::new("/usr/share/doc/grub-efi-amd64/changelog.Debian.gz").is_file() {
+                self.output.log_warn(
+                    "System booted in uefi mode but grub-efi-amd64 meta-package not installed, \
+                     new grub versions will not be installed to /boot/efi!
+                     Install grub-efi-amd64.",
+                )?;
+                boot_ok = false;
+            }
+            if Path::new("/boot/efi/EFI/BOOT/BOOTX64.efi").is_file() {
+                let output = std::process::Command::new("debconf-show")
+                    .arg("--db")
+                    .arg("configdb")
+                    .arg("grub-efi-amd64")
+                    .arg("grub-pc")
+                    .output()
+                    .map_err(|err| format_err!("failed to retrieve debconf settings - {err}"))?;
+                let re = Regex::new(r"grub2/force_efi_extra_removable: +true(?:\n|$)")
+                    .expect("failed to compile dbconfig regex");
+                if !re.is_match(std::str::from_utf8(&output.stdout)?) {
+                    self.output.log_warn(format!(
+                        "Removable bootloader found at '/boot/efi/EFI/BOOT/BOOTX64.efi', but GRUB packages \
+                            not set up to update it!\nRun the following command:\n \
+                            echo 'grub-efi-amd64 grub2/force_efi_extra_removable boolean true' | debconf-set-selections -v -u\n\
+                            Then reinstall GRUB with 'apt install --reinstall grub-efi-amd64'"))?;
+                    boot_ok = false;
+                }
+            }
+            if boot_ok {
+                self.output
+                    .log_pass("bootloader packages installed correctly")?;
+            }
         }
 
         Ok(())

commit b3ef44d11a2c42c0b9f62c4b4c1f9773b2f70907

pbs3to4: fix logic error and typo in log message
/usr/share/doc/systemd-boot/changelog.Debian.gz existing indicates
that system-boot is installed (the `!` was in error).

additionally fix a typo in the log message.

Fixes: 3ae23e71 ("bin: pbs3to4: adapt boot-loader checks to trixie")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Reviewed-by: Shannon Sterz <s.sterz@proxmox.com>
Tested-by: Shannon Sterz <s.sterz@proxmox.com>
Link: https://lore.proxmox.com/20250821141719.4130062-2-s.ivanov@proxmox.com

src/bin/pbs3to4.rs
@@ -207,7 +207,7 @@ impl Checker {
             .log_info("Checking bootloader configuration...")?;
 
         if !Path::new("/sys/firmware/efi").is_dir() {
-            if !Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
+            if Path::new("/usr/share/doc/systemd-boot/changelog.Debian.gz").is_file() {
                 self.output.log_info(
                     "systemd-boot package installed on legacy-boot system is not \
                     necessary, consider removing it",
@@ -215,7 +215,7 @@ impl Checker {
                 return Ok(());
             }
             self.output
-                .log_skip("System booted in legacy-mode - no need for additional pacckages.")?;
+                .log_skip("System booted in legacy-mode - no need for additional packages.")?;
             return Ok(());
         }
 