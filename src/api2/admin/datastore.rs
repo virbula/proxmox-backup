virbula/proxmox-backup

commit 948e84bb627779a0273d02ca5a1825bfe8f3898a

api: datastore: conditionally upload client log to s3 backend
If the datastore is backed by an s3 compatible object store, upload
the client log content to the s3 backend before persisting it to the
local cache store.

Signed-off-by: Christian Ebner <c.ebner@proxmox.com>
Reviewed-by: Lukas Wagner <l.wagner@proxmox.com>
Reviewed-by: Hannes Laimer <h.laimer@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/api2/admin/datastore.rs
@@ -1637,6 +1637,17 @@ pub fn upload_backup_log(
         // always verify blob/CRC at server side
         let blob = DataBlob::load_from_reader(&mut &data[..])?;
 
+        if let DatastoreBackend::S3(s3_client) = datastore.backend()? {
+            let object_key = pbs_datastore::s3::object_key_from_path(
+                &backup_dir.relative_path(),
+                file_name.as_ref(),
+            )
+            .context("invalid client log object key")?;
+            let data = hyper::body::Bytes::copy_from_slice(blob.raw_data());
+            proxmox_async::runtime::block_on(s3_client.upload_replace_with_retry(object_key, data))
+                .context("failed to upload client log to s3 backend")?;
+        };
+
         replace_file(&path, blob.raw_data(), CreateOptions::new(), false)?;
 
         // fixme: use correct formatter

commit 27c2779eaa74408aa1b96408988db309c64b7e48

fix: api: do not block current thread during s3 backup log upload
The api handler for backup task uploads is an async handler,
therefore blocking via proxmox_async::runtime::block_on() is fatal
as will lead to blocking of the current thread, not allowing any
other future to make progress.

Fix this by simply await the s3 api request future instead, as the
api handler is already executed in an async context.

Fixes: https://forum.proxmox.com/threads/169589/
Fixes: 948e84bb ("api: datastore: conditionally upload client log to s3 backend")
Signed-off-by: Christian Ebner <c.ebner@proxmox.com>

src/api2/admin/datastore.rs
@@ -1532,7 +1532,9 @@ pub fn upload_backup_log(
             )
             .context("invalid client log object key")?;
             let data = hyper::body::Bytes::copy_from_slice(blob.raw_data());
-            proxmox_async::runtime::block_on(s3_client.upload_replace_with_retry(object_key, data))
+            s3_client
+                .upload_replace_with_retry(object_key, data)
+                .await
                 .context("failed to upload client log to s3 backend")?;
         };
 