virbula/proxmox-backup

commit 5c5e372451e8b95ad79536e4049911c7358f79c4

pull: refactor source snapshot list filtering logic
In preparation for filter based on the snapshot's verify and
encryption state before applying the transfer last cutoff.

This will allow to correctly use the transfer last filter on the
correspondingly prefiltered items only.

Signed-off-by: Christian Ebner <c.ebner@proxmox.com>
Link: https://lore.proxmox.com/20250729093139.316600-5-c.ebner@proxmox.com

src/server/pull.rs
@@ -613,13 +613,6 @@ async fn pull_group(
         .await?;
     raw_list.sort_unstable_by(|a, b| a.backup.time.cmp(&b.backup.time));
 
-    let total_amount = raw_list.len();
-
-    let cutoff = params
-        .transfer_last
-        .map(|count| total_amount.saturating_sub(count))
-        .unwrap_or_default();
-
     let target_ns = source_namespace.map_prefix(&params.source.get_ns(), &params.target.ns)?;
 
     let mut source_snapshots = HashSet::new();
@@ -633,8 +626,7 @@ async fn pull_group(
     // Also stores if the snapshot is corrupt (verification job failed)
     let list: Vec<(BackupDir, bool)> = raw_list
         .into_iter()
-        .enumerate()
-        .filter_map(|(pos, item)| {
+        .filter_map(|item| {
             let dir = item.backup;
 
             source_snapshots.insert(dir.time);
@@ -666,6 +658,20 @@ async fn pull_group(
                     }
                 }
             }
+            Some((dir, false))
+        })
+        .collect();
+
+    let total_amount = list.len();
+    let cutoff = params
+        .transfer_last
+        .map(|count| total_amount.saturating_sub(count))
+        .unwrap_or_default();
+
+    let list: Vec<(BackupDir, bool)> = list
+        .into_iter()
+        .enumerate()
+        .filter_map(|(pos, (dir, verified))| {
             // Note: the snapshot represented by `last_sync_time` might be missing its backup log
             // or post-backup verification state if those were not yet available during the last
             // sync run, always resync it
@@ -677,7 +683,7 @@ async fn pull_group(
                 transfer_last_skip_info.update(dir.time);
                 return None;
             }
-            Some((dir, false))
+            Some((dir, verified))
         })
         .collect();
 

commit b8e89cf23b0d5cba9b2f4151d1307c5343d65d6c

sync: pull: fix regression in resync of corrupt snapshots
In commit 5c5e3724 ("pull: refactor source snapshot list filtering
logic") the filtering of snapshots to be synced when pulling from the
source datastore was split in preparation for correct pre-filtering
of the snapshot when the verified-only or encrypted-only flags have
been set.

This however introduced a regression, as previously also snapshots
older than the newest snapshot on the target were included in the
list, if the `resync_corrupt` parameter is set and the corresponding
target snapshot failed verification or the loading of the manifest to
check this failed, now however being filtered out unconditionally.

Fix this by keeping these snapshots in the list in case the
`resync_corrupt` flag is set.

Also rename the boolean flag from the incorrect `verified` to
`needs_resync`, in order to reflect the behaviour.

Fixes: 5c5e3724 ("pull: refactor source snapshot list filtering logic")
Signed-off-by: Christian Ebner <c.ebner@proxmox.com>
Reviewed-by: Hannes Laimer <h.laimer@proxmox.com>
Tested-by: Hannes Laimer <h.laimer@proxmox.com>
Link: https://lore.proxmox.com/20250917102957.231963-1-c.ebner@proxmox.com

src/server/pull.rs
@@ -676,7 +676,10 @@ async fn pull_group(
     let list: Vec<(BackupDir, bool)> = list
         .into_iter()
         .enumerate()
-        .filter_map(|(pos, (dir, verified))| {
+        .filter_map(|(pos, (dir, needs_resync))| {
+            if params.resync_corrupt && needs_resync {
+                return Some((dir, needs_resync));
+            }
             // Note: the snapshot represented by `last_sync_time` might be missing its backup log
             // or post-backup verification state if those were not yet available during the last
             // sync run, always resync it
@@ -688,7 +691,7 @@ async fn pull_group(
                 transfer_last_skip_info.update(dir.time);
                 return None;
             }
-            Some((dir, verified))
+            Some((dir, needs_resync))
         })
         .collect();
 