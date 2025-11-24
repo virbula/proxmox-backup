virbula/proxmox-backup

commit b6c5c541828fc0ead919e507ae99430fca8b5dae

ui: traffic-control: add users field in edit form and list
Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>
Reviewed-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Christian Ebner <c.ebner@proxmox.com>
Link: https://lore.proxmox.com/20251121135043.97142-7-h.laimer@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/window/TrafficControlEdit.js
@@ -215,6 +215,7 @@ Ext.define('PBS.window.TrafficControlEdit', {
                 PBS.Utils.delete_if_default(values, 'rate-out');
                 PBS.Utils.delete_if_default(values, 'burst-in');
                 PBS.Utils.delete_if_default(values, 'burst-out');
+                PBS.Utils.delete_if_default(values, 'users');
                 if (typeof values.delete === 'string') {
                     values.delete = values.delete.split(',');
                 }
@@ -276,6 +277,23 @@ Ext.define('PBS.window.TrafficControlEdit', {
         ],
 
         columnB: [
+            {
+                xtype: 'pmxUserSelector',
+                fieldLabel: gettext('Users'),
+                name: 'users',
+                multiSelect: true,
+                allowBlank: true,
+                cbind: {
+                    deleteEmpty: '{!isCreate}',
+                },
+                emptyText: gettext('Applies to all users'),
+                autoEl: {
+                    tag: 'div',
+                    'data-qtip': gettext(
+                        "Limit applies only to authenticated requests by these users. Overrides IP-only rules when both match. If networks are specified on this rule as well, it'll only apply if the users request comes from one of the specified networks.",
+                    ),
+                },
+            },
             {
                 xtype: 'proxmoxtextfield',
                 fieldLabel: gettext('Network(s)'),

www/config/TrafficControlView.js
@@ -182,6 +182,13 @@ Ext.define('PBS.config.TrafficControlView', {
             dataIndex: 'burst-out',
         },
         {
+            header: gettext('Users'),
+            flex: 3,
+            sortable: true,
+            renderer: (users) => (users ? Ext.String.htmlEncode(users.join(', ')) : ''),
+            dataIndex: 'users',
+        },
+        {
             header: gettext('Networks'),
             flex: 3,
             sortable: true,

commit 45395e4573cdaae451a02dc0e6fd3caa10533337

ui: traffic-control: fix `users` field submition
We should not submit an empty string when no user is selected, this
trips up the API endpoint.

Fixes: b6c5c541 ("ui: traffic-control: add users field in edit form and list")
Reported-by: Robert Obkircher <r.obkircher@proxmox.com>
Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>
Link: https://lore.proxmox.com/20251124142107.6240-1-h.laimer@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/window/TrafficControlEdit.js
@@ -283,6 +283,7 @@ Ext.define('PBS.window.TrafficControlEdit', {
                 name: 'users',
                 multiSelect: true,
                 allowBlank: true,
+                skipEmptyText: true,
                 cbind: {
                     deleteEmpty: '{!isCreate}',
                 },